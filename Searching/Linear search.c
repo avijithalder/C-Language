b7a7b901f3c1bb738e1711fefa3fc351a838e929
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-03-27 07:56:40 +0600

    Create Linear search.c

---
+++ Searching/Linear search.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int arr[] = {10, 2, 4, 5, 67, 100, 102};
+
+    int item, pos = -1, i;
+
+    printf("Enter your value ");
+    scanf("%d", &item);
+
+    for (i = 0; i < 7; i++)
+    {
+        if (item== arr[i])
+        {
+            pos = i + 1;
+            break;
+        }
+    }
+
+    if (pos == -1)
+    {
+        printf("Item not found");
+    }
+    else
+    {
+        printf("Item found at index : %d", pos);
+    }
+
+    return 0;
+}

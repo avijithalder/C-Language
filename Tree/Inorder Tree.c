c8347f53e27c7db0af63a7b68938daa45623cfb6
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-05-11 20:12:51 +0600

    Create Inorder Tree.c

---
+++ Tree/Inorder Tree.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    char data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *newNode(char data)
+{
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+void print_In_Order(struct Node *node)
+{
+    if (node == NULL)
+        return;
+    print_In_Order(node->left);
+    printf("%c ", node->data);
+    print_In_Order(node->right);
+}
+
+int main()
+{
+    struct Node *root = newNode('a');
+    root->left = newNode('b');
+    root->right = newNode('c');
+    root->left->left = newNode('d');
+    root->left->right = newNode('e');
+    root->right->left = newNode('f');
+    printf("Traversal of a binary tree: \n");
+    print_In_Order(root);
+    return 0;
+}
